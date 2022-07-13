f2cee629e7525c301faf4786b7a54a33b5e17a99
zy51927042 <zy51927042@163.com>
2022-07-13 23:24:16 +0800

    Add route to beer-service,inventory-service,order-service; Closes #1

---
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,25 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*","/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers**")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082")
+                        .id("inventory-service"))
+                .build();
+
+    }
+}
